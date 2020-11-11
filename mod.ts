db0b2378163ec25b0c98fb0f91ca5ef2827655d8
Xiaojun Gao <gaoxiaojun@gmail.com>
2020-11-12 04:26:00 +0800

    date parse

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
+// deno-lint-ignore-file
 import { Command } from "https://deno.land/x/cliffy/command/command.ts";
 import { printf, sprintf } from "https://deno.land/std/fmt/printf.ts";
-import { cyan, gray, red, yellow } from "https://deno.land/std/fmt/colors.ts";
+import { parse } from "https://deno.land/std/datetime/mod.ts";
 
 const bi5UrlTemplate =
   "https://www.dukascopy.com/datafeed/%s/%04d/%02d/%02d/%02dh_ticks.bi5";
@@ -14,10 +15,23 @@ const { options } = await new Command()
   .option("-s, --symbol <symbol_list>", "Symbol list, split by ','")
   .parse(Deno.args);
 
-if (options.symbol !== undefined) {
-  const url = sprintf(bi5UrlTemplate, options.symbol, 2010, 1, 1, 1);
-  console.log(url);
-}
+// from_date:Date, to_date:Date;
+let from_date = options.from_date !== undefined
+  ? parse(options.from, "yyyy-MM-dd")
+  : new Date();
+let to_date = options.to_date !== undefined
+  ? parse(options.to, "yyyy-MM-dd")
+  : new Date();
+if (options.symbol === undefined) Deno.exit(1);
+const url = sprintf(
+  bi5UrlTemplate,
+  options.symbol,
+  from_date.getFullYear(),
+  from_date.getMonth(),
+  from_date.getDate(),
+  0,
+);
+console.log(url);
 console.log(
   "Symbol: %s from: %s to: %s",
   options.symbol,
