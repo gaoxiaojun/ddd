18446e3fa8a6ded4c6124a602afaec8c21a198ba
Xiaojun Gao <gaoxiaojun@gmail.com>
2020-11-12 03:53:36 +0800

    add dukascopy url template

--- mod.ts
+++ mod.ts
@@ -1,6 +1,11 @@
 import { Command } from "https://deno.land/x/cliffy/command/command.ts";
+import { printf, sprintf } from "https://deno.land/std/fmt/printf.ts";
+import { cyan, gray, red, yellow } from "https://deno.land/std/fmt/colors.ts";
 
-const {options} = await new Command()
+const bi5UrlTemplate =
+  "https://www.dukascopy.com/datafeed/%s/%04d/%02d/%02d/%02dh_ticks.bi5";
+
+const { options } = await new Command()
   .name("duka")
   .version("0.1.0")
   .description("Dukascopy Tickdata Downloader")
@@ -9,4 +14,13 @@ const {options} = await new Command()
   .option("-s, --symbol <symbol_list>", "Symbol list, split by ','")
   .parse(Deno.args);
 
-  console.log("Symbol: %s from: %s to: %s", options.symbol, options.from, options.to);
+if (options.symbol !== undefined) {
+  const url = sprintf(bi5UrlTemplate, options.symbol, 2010, 1, 1, 1);
+  console.log(url);
+}
+console.log(
+  "Symbol: %s from: %s to: %s",
+  options.symbol,
+  options.from,
+  options.to,
+);
